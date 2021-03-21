6de7de69de29fdc177e1edce74d0adf0853cbfdc
M.Alwuhayb <malwuhay@stedwards.edu>
2021-03-21 10:21:08 -0500

    Ma - implment Vehicle class V1

---
+++ Class/Vehicle.py
@@ -0,0 +1,30 @@
+
+# Vehicle class object 
+class Vehicle(object):
+    
+    def _init_(self, id, ServiceType, status, location, model, color):
+        self.id = id
+        self.ServicType = ServicType
+        self.status = status
+        self.location = location
+        self. model = model
+        self. color = color
+
+
+def getId(self):
+    return self.id = id 
+
+def getServiceType(self):
+    return self.ServiceType = ServiceType
+
+def getStatus(self):
+    return self.status = status
+
+def getLocation(self):
+    return self.location = location
+
+def getModel(self):
+    return self.model = model
+
+def getColor(self):
+    return self.color = color
