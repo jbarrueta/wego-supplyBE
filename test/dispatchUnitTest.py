7770712bb1e25e0dce41d7e59360cf3e4423786d
Abigail Ramirez <aramir36@stedwards.edu>
2021-04-18 17:54:19 -0500

    AR iteration of unit test

--- test/dispatchUnitTest.py
+++ test/dispatchUnitTest.py
@@ -5,7 +5,7 @@ import unittest
 
 class TestDispatch(unittest.TestCase):
     def setUp(self):
-        self.dispatch= Dispatch("test2service", "1", "[-97.758911, 30.231760]", "testpass", "2", 
+        self.dispatch= Dispatch("test2service", "1", "[-97.758911, 30.231760]", "2", 
                                 "[[-97.758917, 30.231775], [-97.758166, 30.232976], [-97.757988,30.232891], [-97.757622, 30.232621], [-97.757501, 30.232374], [-97.757427, 30.23205]]")
 
 class TestInit(TestDispatch):
@@ -24,25 +24,25 @@ class TestInit(TestDispatch):
     def test_route(self):
         self.assertEqual(self.dispatch.route, "[[-97.758917, 30.231775], [-97.758166, 30.232976], [-97.757988,30.232891], [-97.757622, 30.232621], [-97.757501, 30.232374], [-97.757427, 30.23205]]")
 
-class TestVehicle(Dispatch):
+class TestVehicle(TestDispatch):
     def test_assignVehicle(self):
         self.assertEqual(self.dispatch.vehicle_id, "2")
 
     def test_getAssignedVehicle(self):
         self.assertEqual(self.dispatch.getAssignedVehicle, "2")
 
-class TestRoute(Dispatch):
+class TestRoute(TestDispatch):
     def test_setRoute(self):
         self.assertEqual(self.dispatch.route, "[[-97.758917, 30.231775], [-97.758166, 30.232976], [-97.757988,30.232891], [-97.757622, 30.232621], [-97.757501, 30.232374], [-97.757427, 30.23205]]")
 
     def test_getCurrentRoute(self):
         self.assertEqual(self.dispatch.route, "[[-97.758917, 30.231775], [-97.758166, 30.232976], [-97.757988,30.232891], [-97.757622, 30.232621], [-97.757501, 30.232374], [-97.757427, 30.23205]]")
 
-class TestCoordinates(self):
+class TestCoordinates(TestDispatch):
     def test_getOrderCoordinates(self):
         self.assertEqual(self.dispatch.order_coords, "[-97.758911, 30.231760]")
 
-class TestFleetId(self):
+class TestFleetId(TestDispatch):
     def test_getFleetId(self):
         self.assertEqual(self.dispatch.getFleetId, "test2service")
 
