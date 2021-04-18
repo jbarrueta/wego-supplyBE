ba8fdbc0f5f593a622f5c393d3d5e10992ae09f6
Abigail Ramirez <aramir36@stedwards.edu>
2021-04-18 14:07:18 -0500

    AR Iteration of dispatch, fleet manager, and vehicle

--- unitTests/fleet.py
+++ unitTests/fleet.py
@@ -3,18 +3,17 @@ import unittest
 
 class TestFleet(unittest.TestCase):
     def setUp(self):
-        self.fleet = Fleet("test@test.com", "Testf", "Testl", "testpass")
-        #self.fleet = Fleet("The Fleet")
+        self.fleet = Fleet("test2service", "1")
 
 class TestInit(TestFleet):
-    def test_setId(self):
-        self.assertEqual(self.fleet.service_type, "test@test.com")
-        self.assertEqual(self.fleet._id, "test@test.com")
+    def test_init(self):
+        self.assertEqual(self.fleet.service_type, "servicetest")
+        self.assertEqual(self.fleet._id, "1")
 
 # test for the method setId
 class TestSetId(TestFleet):
     def test_setId(self):
-        self.assertEqual(self.fleet.setId(), "_id object")
+        self.assertEqual(self.fleet.setId(), "1")
 
 
 if __name__ == '__main__':
